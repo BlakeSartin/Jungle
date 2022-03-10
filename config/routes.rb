7973e99be9861fca2873ddc172816d018496fe82
Blake Sartin <blake.sartin8@gmail.com>
2022-03-10 22:30:57 +0000

    add dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   resources :orders, only: [:create, :show]
 
   namespace :admin do
-    root to: 'dashboard#show'
+    root to: 'dashboard#index'
     resources :products, except: [:edit, :update, :show]
   end
 
