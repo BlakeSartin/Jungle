662fc2762eaa1d91c0f178cddbc7d0df50dfcbdd
Blake Sartin <blake.sartin8@gmail.com>
2022-03-15 21:50:49 +0000

    fixed login bug and cart spec bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,10 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true, :uniqueness => {:case_sensitive => false}
   validates :password, presence: true, length: {minimum: 6}
-  validates :password_confirmation, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
   
   def self.authenticate_with_credentials(email, password)
-    pp "here", email, password
-    email = email.downcase.strip
-    user = self.find_by_email(email)
-    pp "user", user, user.authenticate(password)
+    user = User.find_by_email(email.strip.downcase)
     if user && user.authenticate(password)
       user
     else
